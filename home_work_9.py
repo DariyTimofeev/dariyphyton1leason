3d4cd62ababc16e05468e7e6e6be6662b9665ab1
Admin <ffefe@gmail.com>
2024-12-11 11:56:25 +0200

    ДЗ N9

---
+++ home_work_9.py
@@ -0,0 +1,24 @@
+from pywebio.input import input,NUMBER
+from pywebio.output import put_text
+
+def calculate_ticket_price():
+    while True:
+
+        age = input("Введіть свій вік (або натисніть Enter для виходу):", type=NUMBER)
+
+        if age is None:
+            break
+
+        if age < 6:
+            price = "Безкоштовно"
+        elif 6 <= age <= 12:
+            price = "50 грн (знижка 50%)"
+        elif 13 <= age <= 17:
+            price = "75 грн (знижка 25%)"
+        elif 18 <= age < 60:
+            price = "100 грн (повна ціна)"
+        else:  # age >= 60
+            price = "70 грн (знижка 30%)"
+
+        put_text(f"Вартість квитка: {price}")
+calculate_ticket_price()
