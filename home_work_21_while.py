e1b60724b1120c3515dcfdb13b609ac158ebb798
Admin <ffefe@gmail.com>
2025-01-21 17:44:50 +0200

    ДЗ N21

---
+++ home_work_21_while.py
@@ -0,0 +1,10 @@
+def calculate_sum():
+    total = 0
+    while True:
+        number = int(input("Введіть число: "))
+        if number == 0:
+            break
+        total += number
+    print(f"Сума чисел: {total}")
+
+calculate_sum()
