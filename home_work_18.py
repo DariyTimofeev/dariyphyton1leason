b39a795fd2146155a6a0500684569852386b85bf
Admin <ffefe@gmail.com>
2025-01-23 20:03:59 +0200

    ДЗ N18

---
+++ home_work_18.py
@@ -0,0 +1,8 @@
+import csv
+
+with open('csv', newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=';')
+
+    for row in reader:
+        if row['iso_country'] == 'UA':
+            print(row['name'])
