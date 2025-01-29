3d1f9f325e7a066c4fb27553784e4d4f2f7c81f5
Admin <ffefe@gmail.com>
2025-01-29 19:47:32 +0200

    ДЗ N18

--- home_work_18.py
+++ home_work_18.py
@@ -1,8 +1,8 @@
 import csv
 
+# Читаємо файл і виводимо тільки аеропорти з України
 with open('csv', newline='', encoding='utf-8') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=';')
-
+    reader = csv.DictReader(csvfile, delimiter=';');
     for row in reader:
         if row['iso_country'] == 'UA':
-            print(row['name'])
+            print(row['name']);
